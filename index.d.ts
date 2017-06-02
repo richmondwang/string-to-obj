e6f960952442151491ca6b81be85d27d9e56ef2b
VJ Cagay <vjcagay@gmail.com>
2017-06-02 11:30:09 +0900

    :art: Add TypeScript definitions

---
+++ index.d.ts
@@ -0,0 +1,23 @@
+interface stringToObjConfig {
+  trim: Boolean;
+  delimiters: {
+    values: { [key: string]: any },
+    blackhole: string | Boolean,
+  };
+}
+
+declare module 'string-to-obj' {
+
+  class stringToObj {
+    constructor(config: stringToObjConfig);
+  }
+
+  namespace stringToObj {
+    function parse(input: string): { [key: string]: string[] };
+    const source: stringToObjConfig;
+  }
+
+  export = stringToObj;
+
+}
+
